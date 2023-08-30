56580e7308aac3896392800ad3a27f0e828eddcb
sgrazys <s.grazys@gmail.com>
2023-08-30 23:40:20 +0300

    add card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,15 +1,20 @@
 function Card({ feature }) {
 	return (
 		<li className='card'>
-			<div className='card__content'>
-				<h2 className='card__title'>{feature.title}</h2>
-				<p className='card__description'>{feature.description}</p>
+			<div
+				className='card__container'
+				style={{ borderTop: `3.55px solid ${feature.color}` }}
+			>
+				<div className='card__content'>
+					<h2 className='card__title'>{feature.title}</h2>
+					<p className='card__description'>{feature.description}</p>
+				</div>
+				<img
+					className='card__icon'
+					src={feature.icon}
+					alt='PLACEHOLDER'
+				/>
 			</div>
-			<img
-				className='card__icon'
-				src={feature.icon}
-				alt='PLACEHOLDER'
-			/>
 		</li>
 	);
 }
