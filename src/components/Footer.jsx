ec1b2a2dc1d1668c0da77125266c8391044a94b3
sgrazys <s.grazys@gmail.com>
2023-08-30 23:39:58 +0300

    add footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,23 @@
+function Footer() {
+	return (
+		<footer>
+			<p className='attribution'>
+				Challenge by{' '}
+				<a
+					href='https://www.frontendmentor.io/challenges/four-card-feature-section-weK1eFYK'
+					target='_blank'
+					rel='noreferrer'
+				>
+					Frontend Mentor
+				</a>
+				. Coded by{' '}
+				<a href='https://www.frontendmentor.io/profile/sgrazys'>
+					Saulius Grazys
+				</a>
+				.
+			</p>
+		</footer>
+	);
+}
+
+export default Footer;
